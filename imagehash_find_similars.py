5f4db8de248826081a93244b473a1506838ef542
saramsv <sara.mousavi.ch@gmail.com>
2019-04-08 21:29:57 -0400

    Update imagehash_find_similars.py

--- imagehash_find_similars.py
+++ imagehash_find_similars.py
@@ -1,17 +1,40 @@
-# we got image hash from https://github.com/JohannesBuchner/imagehash
-#run it: python3 imagehash_find_similars.py images/1.JPG images
-
 from PIL import Image
 import imagehash
 import glob
 import sys
+import csv
+import pickle
+import os
 
+#to read in the dict that I made
+'''
+pickle_in = open("dict.hashes","rb")
+example_dict = pickle.load(pickle_in)
+'''
 
-img_name = sys.argv[1]
-img_dir = sys.argv[2]
+img_paths = sys.argv[1]
+hashes = {}
 
-hash_base = imagehash.average_hash(Image.open(img_name), 8)
-for name in glob.glob(img_dir + '/*.JPG'):
-    print(name)
-    hash_comp = imagehash.average_hash(Image.open(name), 8)
-    print(hash_base - hash_comp)
+with open (img_paths, 'r') as f:
+    names= f.readlines()
+    for name in names:
+        name = name[name.find('/ic'): name.find('JPG')+len('JPG')]
+        if os.path.isfile(name):
+            try:
+                img_hash = imagehash.average_hash(Image.open(name), 8)
+                print(str(name) + ';' + str(img_hash))
+                hashes[name] = img_hash
+            except OSError:
+                continue
+
+# To write in a dict format
+pickle_out = open("dict.hash","wb")
+pickle.dump(hashes, pickle_out)
+pickle_out.close()
+
+# To write in a csv format
+'''
+w = csv.writer(open("dict_hash", "w"))
+for key, val in hashes.items():
+    w.writerow([key, val])
+'''
